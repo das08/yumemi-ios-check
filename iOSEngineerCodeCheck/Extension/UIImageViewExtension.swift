8d28d8039b1e10eca553c59c0067eb5e583e2340
das08 <takeda5955@gmail.com>
2022-02-18 18:49:11 +0900

    add: extension for UIImageView to crop image in circle

---
+++ iOSEngineerCodeCheck/Extension/UIImageViewExtension.swift
@@ -0,0 +1,19 @@
+//
+//  UIImageViewExtension.swift
+//  iOSEngineerCodeCheck
+//
+//  Created by Kazuki Takeda on 2022/02/18.
+//  Copyright © 2022 YUMEMI Inc. All rights reserved.
+//
+
+import UIKit
+
+extension UIImageView {
+
+    func makeRound() {
+        self.layer.borderWidth = 1
+        self.layer.masksToBounds = false
+        self.layer.cornerRadius = self.frame.height / 2
+        self.clipsToBounds = true
+    }
+}
