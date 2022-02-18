e4468420b73731f22a3495ecaeefdb4e4436f977
das08 <takeda5955@gmail.com>
2022-02-18 21:06:52 +0900

    fix: re-created main view since xcode is not recognizing

--- iOSEngineerCodeCheck/View/RepositorySearchMainView.swift
+++ iOSEngineerCodeCheck/View/RepositorySearchMainView.swift
@@ -1,15 +1,16 @@
 //
-//  RepositorySearchView.swift
+//  RepositorySearchMainView.swift
 //  iOSEngineerCodeCheck
 //
-//  Created by Kazuki Takeda on 2022/02/17.
+//  Created by Kazuki Takeda on 2022/02/18.
 //  Copyright © 2022 YUMEMI Inc. All rights reserved.
 //
 
 import UIKit
 
-class RepositorySearchView: UITableViewController {
+class RepositorySearchMainView: UITableViewController {
     
+    @IBOutlet weak var navigationBar: UINavigationItem!
     @IBOutlet weak var searchBar: UISearchBar!
     
     private var presenter: RepositorySearchPresenter!
@@ -19,6 +20,8 @@ class RepositorySearchView: UITableViewController {
         // Do any additional setup after loading the view.
         searchBar.placeholder = "GitHubのリポジトリを検索できるよー"
         searchBar.delegate = self
+        navigationBar.title = "Discover!"
+        navigationBar.backBarButtonItem?.title = nil
         presenter = RepositorySearchPresenter.init(with: self)
     }
     
@@ -54,7 +57,7 @@ class RepositorySearchView: UITableViewController {
     }
 }
 
-extension RepositorySearchView: RepositorySearchPresenterOutput {
+extension RepositorySearchMainView: RepositorySearchPresenterOutput {
     func didFetch(_ repositories: [Repository]) {
         DispatchQueue.main.async {
             self.tableView.reloadData()
@@ -71,7 +74,7 @@ extension RepositorySearchView: RepositorySearchPresenterOutput {
     }
 }
 
-extension RepositorySearchView: UISearchBarDelegate {
+extension RepositorySearchMainView: UISearchBarDelegate {
     func searchBarShouldBeginEditing(_ searchBar: UISearchBar) -> Bool {
         return true
     }
